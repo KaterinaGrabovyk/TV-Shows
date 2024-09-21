b3c3f939f0160cd5db81b80d65ea0b747f1d16f2
KaterinaGrabovyk <katya.grabovik@ukr.net>
2024-09-21 17:34:16 +0300

    new styles,view and random functions

--- index.js
+++ index.js
@@ -1,41 +1,85 @@
-import express from "express"
-import axios from "axios"
+import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
 
-const port=3000;
-const app=express();
+const port = 3000;
+const app = express();
 //pages 0-318
-let t=1;
+let t = 1;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //about page
-app.get("/",(req,res)=>{
-res.render("index.ejs");
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 //first page
-app.get("/Shows",async(req,res)=>{
-  t=1;
-        try {
-                const response = await axios.get(`https://api.tvmaze.com/shows?page=${t-1}`);
-                const result = response.data;
-                console.log(`page: 1`);
-                res.render("shows.ejs" ,{ shows: result,pageNumber:1});
-              } catch (error) {
-                console.error("Failed to make request:", error.message);
-                res.render("index.ejs", {
-                  error: error.message,
-                });
-              }
-})
-//view show info
-app.get("/Shows/:id",async(req,res)=>{
+app.get("/Shows", async (req, res) => {
   try {
-    const response = await axios.get(`https://api.tvmaze.com/shows?page=${t-1}`);
+    const response = await axios.get(
+      `https://api.tvmaze.com/shows?page=${t - 1}`
+    );
     const result = response.data;
-    let obj = result.filter(res => res.id === parseInt(req.params.id));
-    res.render("show.ejs",{im:obj[0].image,title:obj[0].name});
+    console.log(`page: 1`);
+    res.render("shows.ejs", { shows: result, pageNumber: t });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+//view show info
+app.get("/Show/:id", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.tvmaze.com/shows?page=${t - 1}`
+    );
+    const response2 = await axios.get(
+      `https://api.tvmaze.com/shows/${req.params.id}/cast`
+    );
+    const result = response.data;
+    const cast = response2.data;
+    let obj = result.filter((res) => res.id === parseInt(req.params.id));
+    res.render("show.ejs", { show: obj[0], actors: cast });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+//change pages
+app.post("/Shows", async (req, res) => {
+  t = parseInt(req.body.pageNumber, 10);
+  const action = req.body.action;
+
+  if (action === "prev" && t > 0) {
+    t--;
+  } else if (action === "next" && t < 319) {
+    t++;
+  } else if (action === "start") {
+    t = 1;
+  } else if (action === "end") {
+    t = 319;
+  }
+  console.log(`page: ${t}`);
+  const response = await axios.get(
+    `https://api.tvmaze.com/shows?page=${t - 1}`
+  );
+  const result = response.data;
+  res.render("shows.ejs", { shows: result, pageNumber: t });
+});
+app.get("/Random",async(req,res)=>{
+  let random=Math.floor(Math.random()*319);
+  try {
+    const response = await axios.get(
+      `https://api.tvmaze.com/shows?page=${random}`
+    );
+    const result = response.data;
+    let r=Math.floor(Math.random()*result.length);
+    res.render("random.ejs", { show: result[r] });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -43,27 +87,6 @@ app.get("/Shows/:id",async(req,res)=>{
     });
   }
 })
-
-//change pages
-app.post("/Shows", async (req, res) => {
-        t = parseInt(req.body.pageNumber, 10); 
-        const action = req.body.action;
-    
-        if (action === 'prev' && t > 0) {
-            t--; 
-        } else if (action === 'next' && t < 319) {
-            t++; 
-        }else if (action === 'start') {
-          t=1; 
-      }else if (action === 'end') {
-          t=319; 
-      }
-        console.log(`page: ${t}`);
-       const response = await axios.get(`https://api.tvmaze.com/shows?page=${t-1}`);
-       const result = response.data;
-       res.render("shows.ejs" ,{ shows: result, pageNumber:t});
-    });
-
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+  console.log(`Listening on port ${port}`);
+});
